1934342f4a450a67d2633f21d8229984f7fb9a50
Bhargav Rasesh Patel <A00303747@canadahanson.ca>
2024-10-17 10:07:49 -0400

    make private db connection

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./configs/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://Gemini%20Al_owner:WbOu4o0wkNxL@ep-old-smoke-a58kydex.us-east-2.aws.neon.tech/Third-Sem-Project?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
     }
   };
